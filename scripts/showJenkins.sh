897d518dfec5c27f3ca9adf61de33a22c08682b4
rob jahn <robjahn@gmail.com>
2019-03-20 08:38:13 -0400

    enhance

--- scripts/showJenkins.sh
+++ scripts/showJenkins.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+export JENKINS_USER=$(cat creds.json | jq -r '.jenkinsUser')
+export JENKINS_PASSWORD=$(cat creds.json | jq -r '.jenkinsPassword')
 export JENKINS_URL=$(kubectl describe svc jenkins -n cicd | grep "LoadBalancer Ingress:" | sed 's~LoadBalancer Ingress:[ \t]*~~')
-export JENKINS_URL_PORT=(cat creds.json | jq -r '.jenkinsPort')
+export JENKINS_URL_PORT=$(cat creds.json | jq -r '.jenkinsPort')
 
-echo "----------------------------------------------------"
+echo "--------------------------------------------------------------------------"
 echo "Jenkins is running @ : http://$JENKINS_URL:$JENKINS_URL_PORT"
 echo "Admin user           : $JENKINS_USER"
 echo "Admin password       : $JENKINS_PASSWORD"
+echo "--------------------------------------------------------------------------"
+echo ""
