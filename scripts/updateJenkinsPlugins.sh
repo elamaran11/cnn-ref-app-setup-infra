cf3118fc22116442a160c36527949cc276798fab
Mike Villiger <akirasoft@gmail.com>
2019-03-20 18:00:48 -0500

    oops, the update all jenkins plugin script isnt right yet

--- scripts/updateJenkinsPlugins.sh
+++ scripts/updateJenkinsPlugins.sh
@@ -5,8 +5,8 @@ export JENKINS_PASSWORD=$(cat creds.json | jq -r '.jenkinsPassword')
 
 case $1 in
   all)
-    echo "updating all installed plugins on $JENKINS_POD"
-    kubectl -n cicd exec -it $JENKINS_POD -- sh -c "java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ -auth $JENKINS_USER:$JENKINS_PASSWORD list-plugins | grep -e ')$' | awk '{ print $1 }'"
+    echo "updating all installed plugins on $JENKINS_POD - still working on this one"
+#    kubectl -n cicd exec -it $JENKINS_POD -- sh -c "updates=$(java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ -auth $JENKINS_USER:$JENKINS_PASSWORD list-plugins | grep -e ')$' | awk '{ print $1 }') && java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ -auth $JENKINS_USER:$JENKINS_PASSWORD install-plugin ${updates} && java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ -auth $JENKINS_USER:$JENKINS_PASSWORD safe-restart"
     ;;
   just-perfsig)
     echo "just updating perfsig plugins on $JENKINS_POD"
