6f88d659507bf3ce7f2469a23c676abecba6d84e
peterhack <peter.hack@dynatrace.com>
2019-03-28 22:41:20 -0400

    added configuration for alternate deployments

--- scripts/validatePrerequisites.sh
+++ scripts/validatePrerequisites.sh
@@ -4,86 +4,96 @@ LOG_LOCATION=./logs
 exec > >(tee -i $LOG_LOCATION/validatePrerequisites.log)
 exec 2>&1
 
-echo "----------------------------------------------------"
-echo Validating pre-requisites
-echo "----------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "                     Validating pre-requisites"
+echo "-----------------------------------------------------------------"
+echo ""
 
-echo -n "Validating jq utility ... "
+echo -n "Validating jq utility				"
 type jq &> /dev/null
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "Error"
     echo ">>> Missing 'jq' json query utility"
     echo ""
     exit 1
 fi
-echo "ok"
+echo "ok	$(command -v jq)"
 
-echo -n "Validating hub utility ... "
+echo -n "Validating hub utility				"
 type hub &> /dev/null
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "Error"
     echo ">>> Missing git 'hub' utility"
     echo ""
     exit 1
 fi
-echo "ok"
+echo "ok	$(command -v hub)"
 
-echo -n "Validating kubectl ... "
+echo -n "Validating kubectl				"
 type kubectl &> /dev/null
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "Error"
     echo ">>> Missing 'kubectl'"
     echo ""
     exit 1
 fi
-echo "ok"
+echo "ok	$(command -v kubectl)"
 
-echo -n "Validating AWS cli ... "
-type aws &> /dev/null
-if [ $? -ne 0 ]
-then
+# Validating for OCP
+if [ $DEPLOYMENT == ocp ]; then
+  echo -n "Validating oc				"
+  type oc &> /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Error"
+    echo ">>> Missing 'oc'"
+    echo ""
+    exit 1
+  fi
+  echo "ok	$(command -v oc)"
+fi
+
+# Validating for eks
+if [ $DEPLOYMENT == eks ]; then
+  echo -n "Validating AWS cli				"
+  type aws &> /dev/null
+  if [ $? -ne 0 ]; then
     echo "Error"
     echo ">>> Missing 'aws CLI'"
     echo ""
     exit 1
-fi
-echo "ok"
+  fi
+  echo "ok	$(command -v aws)"
 
-echo -n "Validating terraform ... "
-type aws &> /dev/null
-if [ $? -ne 0 ]
-then
+  echo -n "Validating terraform				"
+  type terraform &> /dev/null
+  if [ $? -ne 0 ]; then
     echo "Error"
     echo ">>> Missing 'terraform'"
     echo ""
     exit 1
-fi
-echo "ok"
+  fi
+  echo "ok	$(command -v terraform)"
 
-echo -n "Validating aws-iam-authenticator utility ... "
-type aws-iam-authenticator &> /dev/null
-if [ $? -ne 0 ]
-then
+  echo -n "Validating aws-iam-authenticator utility	"
+  type aws-iam-authenticator &> /dev/null
+  if [ $? -ne 0 ]; then
     echo "Error"
     echo ">>> Missing 'aws-iam-authenticator'"
     echo ""
     exit 1
-fi
-echo "ok"
+  fi
+  echo "ok	$(command -v aws-iam-authenticator)"
 
-echo -n "Validating AWS cli is configured ... "
-export AWS_STS_USER=$(aws sts get-caller-identity | jq -r '.UserId')
-if [ -z $AWS_STS_USER ]
-then
+  echo -n "Validating AWS cli is configured		"
+  export AWS_STS_USER=$(aws sts get-caller-identity | jq -r '.UserId')
+  if [ -z $AWS_STS_USER ]; then
     echo ">>> Unable to locate credentials. You can configure credentials by running \"aws configure\"."
     echo ""
     exit 1
+  fi
+  echo "ok	AWS cli is configured with UserId: $AWS_STS_USER"
 fi
-echo "...ok - AWS cli is configured with UserId: $AWS_STS_USER"
-
-echo "----------------------------------------------------"
-echo Validation of pre-requisites complete.
-echo "----------------------------------------------------"
+  
+echo "-----------------------------------------------------------------"
+echo "           Validation of pre-requisites complete"
+echo "-----------------------------------------------------------------"
